a563fd87f8928f68a511b13551f55db980643d50
Yevhenii Volosiuk <volosiukweb@gmail.com>
2023-06-05 19:30:36 +0200

    add first relationships

--- app/Models/Employee.php
+++ app/Models/Employee.php
@@ -4,6 +4,7 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
 use Illuminate\Database\Eloquent\SoftDeletes;
 
 class Employee extends Model
@@ -22,4 +23,24 @@ class Employee extends Model
         'birthdate',
         'date_hired',
     ];
+
+    public function state(): belongsTo
+    {
+        return $this->belongsTo(State::class);
+    }
+
+    public function city(): belongsTo
+    {
+        return $this->belongsTo(City::class);
+    }
+
+    public function department(): belongsTo
+    {
+        return $this->belongsTo(Department::class);
+    }
+
+    public function country(): belongsTo
+    {
+        return $this->belongsTo(Country::class);
+    }
 }
--- app/Models/User.php
+++ app/Models/User.php
@@ -4,6 +4,7 @@ namespace App\Models;
 
 // use Illuminate\Contracts\Auth\MustVerifyEmail;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
 use Illuminate\Database\Eloquent\SoftDeletes;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
@@ -35,6 +36,11 @@ class User extends Authenticatable
         'password',
         'remember_token',
     ];
+
+    public function employee(): belongsTo
+    {
+        return $this->belongsTo(Employee::class);
+    }
 
     /**
      * The attributes that should be cast.
