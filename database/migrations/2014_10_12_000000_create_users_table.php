25b53e119beb2e7eb8930095efa7d769c4202d83
Yevhenii Volosiuk <volosiukweb@gmail.com>
2023-06-05 18:15:14 +0200

    edit user table columns

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -13,12 +13,15 @@ return new class extends Migration
     {
         Schema::create('users', function (Blueprint $table) {
             $table->id();
-            $table->string('name');
-            $table->string('email')->unique();
+            $table->string('username', 20);
+            $table->string('first_name', 60);
+            $table->string('last_name', 60);
+            $table->string('email', 60)->unique();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
             $table->rememberToken();
             $table->timestamps();
+            $table->softDeletes();
         });
     }
 
