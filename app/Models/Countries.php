9ff0152c32f871403bcea143b2c9fea1a1c06251
Yevhenii Volosiuk <volosiukweb@gmail.com>
2023-06-05 18:36:34 +0200

    add country model & migration

---
+++ app/Models/Countries.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
+
+class Countries extends Model
+{
+    use HasFactory, softDeletes;
+
+    protected $table = 'countries';
+
+    protected $fillable = [
+        'country_code',
+        'name',
+    ];
+}
