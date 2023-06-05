434307ed554916522dd2960a8b322c417bc9b063
Yevhenii Volosiuk <volosiukweb@gmail.com>
2023-06-05 18:46:17 +0200

    add length for name column

--- database/migrations/2023_06_05_163759_create_states_table.php
+++ database/migrations/2023_06_05_163759_create_states_table.php
@@ -13,7 +13,7 @@ return new class extends Migration {
         Schema::create('states', function (Blueprint $table) {
             $table->id();
             $table->foreignId('country_id')->constrained();
-            $table->string('name');
+            $table->string('name', 60);
             $table->timestamps();
             $table->softDeletes();
         });
